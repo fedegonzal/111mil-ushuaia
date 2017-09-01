38ea004c9f43254dc93da5ff91fd3023c7b4bdaa
Federico González Brizzio <fedegonzal@gmail.com>
2017-08-31 23:25:30 -0300

    100 inmuebles de ejemplo para trabajar con los ejercicios

---
+++ ejercicios/clase 04/inmuebles.js
@@ -0,0 +1,702 @@
+var inmuebles = [
+  {
+    "localidad": "Santa Fe",
+    "calle": "Iturraspe",
+    "numero": 2405,
+    "operacion": "Alquiler",
+    "precio": 8500
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "1ra Junta",
+    "numero": 4845,
+    "operacion": "Compra",
+    "precio": 5500000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "1ra Junta",
+    "numero": 4845,
+    "operacion": "Alquiler",
+    "precio": 35000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Primera Junta",
+    "numero": 4861,
+    "operacion": "Compra",
+    "precio": 3500000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Primera Junta",
+    "numero": 4861,
+    "operacion": "Alquiler",
+    "precio": 30000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Suipacha",
+    "numero": "2423 Piso 1 of 3",
+    "operacion": "Compra",
+    "precio": 850000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Ctda.Bustamante",
+    "numero": "2215 P.1 D.5",
+    "operacion": "Alquiler",
+    "precio": 4000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Corrientes",
+    "numero": "2651 U.12",
+    "operacion": "Compra",
+    "precio": 320000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de enero",
+    "numero": 2089,
+    "operacion": "Alquiler",
+    "precio": 6000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Mendoza",
+    "numero": "2905 P.2 D.3",
+    "operacion": "Alquiler",
+    "precio": 3800
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Derqui",
+    "numero": 2196,
+    "operacion": "Compra",
+    "precio": 1800000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Grand Bourg",
+    "numero": 4301,
+    "operacion": "Compra",
+    "precio": 200000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Junin",
+    "numero": "2437 P.5",
+    "operacion": "Compra",
+    "precio": 120000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4664 T.1 P.4 D B",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4664 T.1 P.6 D.A",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4664 T.1 P.5 D.A",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "San Jose",
+    "numero": "2254 P.1 D.4",
+    "operacion": "Alquiler",
+    "precio": 6000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Patricio Cullen",
+    "numero": 6519,
+    "operacion": "Compra",
+    "precio": 2200000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Publica",
+    "numero": "S/N",
+    "operacion": "Compra",
+    "precio": 400000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Estanislao del Campo",
+    "numero": "2728 Altos y Bajos",
+    "operacion": "Alquiler",
+    "precio": 7500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Pasaje Dean Funes",
+    "numero": 2818,
+    "operacion": "Compra",
+    "precio": 2500000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.5 Confrafrente Norte",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.5 Contrafrente Sur",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.5 Frente Sur",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.5 Frente Norte",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.4 Frente Norte",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.4 Frente Sur",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.4 Contrafrente Sur",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.4 Contrafrente Norte",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.3 Contrafrente Norte",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.3 Contrafrente Sur",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.3 Frente Sur",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.3 Frente Norte",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.2 Frente Norte",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.2 Frente Sur",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 Contrafrente Sur",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.2 Contrafrente Norte",
+    "operacion": "Alquiler",
+    "precio": 6300
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.1 Contrafrente Norte",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.F.Zuviria",
+    "numero": "4937 P.1 Contrafrente Sur",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "2 Lagunas Barrio Privado",
+    "numero": "Lote NÂº85",
+    "operacion": "Compra",
+    "precio": 80000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Saavedra",
+    "numero": 5181,
+    "operacion": "Compra",
+    "precio": 100000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Corrientes",
+    "numero": "2903 Dto.2 Planta Alta-Frente",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Alberdi",
+    "numero": "3050 D.1",
+    "operacion": "Compra",
+    "precio": 1600000
+  },
+  {
+    "localidad": "Colastine Norte",
+    "calle": "Los Zorzales",
+    "numero": "6149 -Ruta 1 Km 2,5",
+    "operacion": "Alquiler",
+    "precio": 4000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Rivadavia",
+    "numero": "2553 Piso 1 Oficina 81",
+    "operacion": "Alquiler",
+    "precio": 3000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Juan de Garay",
+    "numero": 1262,
+    "operacion": "Compra",
+    "precio": 2800000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "RUTA N.1",
+    "numero": "KM 8.5",
+    "operacion": "Compra",
+    "precio": 4100000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Moreno",
+    "numero": 2763,
+    "operacion": "Compra",
+    "precio": 155000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Eva Peron",
+    "numero": "2647 Piso 5 Dto.A-Frente",
+    "operacion": "Alquiler",
+    "precio": 7900
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Hipolito Irigoyen",
+    "numero": "2609 Piso 3 Dto.2",
+    "operacion": "Alquiler",
+    "precio": 7500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Corrientes",
+    "numero": 2733,
+    "operacion": "Compra",
+    "precio": 590000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de Enero",
+    "numero": 3734,
+    "operacion": "Compra",
+    "precio": 4000000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av.A.del Valle",
+    "numero": 9200,
+    "operacion": "Alquiler",
+    "precio": 40000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de Enero",
+    "numero": "2850 P.4 D.3",
+    "operacion": "Compra",
+    "precio": 1600000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Av. Lujan",
+    "numero": 2200,
+    "operacion": "Compra",
+    "precio": 4800000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Alte. Brown",
+    "numero": 1778,
+    "operacion": "Compra",
+    "precio": 2100000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Guemes",
+    "numero": 2036,
+    "operacion": "Compra",
+    "precio": 3100000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Ulibarrie",
+    "numero": 1113,
+    "operacion": "Compra",
+    "precio": 2000000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Eva Peron",
+    "numero": "2409 Piso 1- COCHERA 30",
+    "operacion": "Alquiler",
+    "precio": 1000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Bv. Pellegrini",
+    "numero": 2862,
+    "operacion": "Compra",
+    "precio": 1700000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Eva Peron",
+    "numero": "2409-Piso 3 - COCHERA 94",
+    "operacion": "Alquiler",
+    "precio": 1200
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Eva Peron",
+    "numero": "2409-Piso 3 - COCHERA 95",
+    "operacion": "Alquiler",
+    "precio": 1200
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Eva Peron",
+    "numero": "2409-Piso 1 - COCHERA 69",
+    "operacion": "Alquiler",
+    "precio": 1200
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de Enero",
+    "numero": "2850 Cochera 9- Subsuelo",
+    "operacion": "Alquiler",
+    "precio": 700
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Salta",
+    "numero": 2562,
+    "operacion": "Compra",
+    "precio": 200000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "San Martin",
+    "numero": "2753 COCHERA 31",
+    "operacion": "Alquiler",
+    "precio": 1150
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Primera Junta",
+    "numero": 2515,
+    "operacion": "Alquiler",
+    "precio": 16000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Primera Junta",
+    "numero": "2507 Piso 3- Oficina 8",
+    "operacion": "Alquiler",
+    "precio": 2600
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Rivadavia",
+    "numero": "2861  L.3",
+    "operacion": "Compra",
+    "precio": 90000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Espora",
+    "numero": 2869,
+    "operacion": "Compra",
+    "precio": 1500000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Cordoba",
+    "numero": 3247,
+    "operacion": "Compra",
+    "precio": 235000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de Enero",
+    "numero": "2850 P.6 D.3",
+    "operacion": "Compra",
+    "precio": 1550000
+  },
+  {
+    "localidad": "Desvio Arijon",
+    "calle": "España",
+    "numero": "casi Salta",
+    "operacion": "Compra",
+    "precio": 890000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "San Martin",
+    "numero": "2171 Local 2-P.Baja-Galeria Sol Garden",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "matheu",
+    "numero": "s/n",
+    "operacion": "Compra",
+    "precio": 450000
+  },
+  {
+    "localidad": "Sauce Viejo",
+    "calle": "El mate",
+    "numero": 1287,
+    "operacion": "Alquiler",
+    "precio": 6000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "San Jeronimo",
+    "numero": "1800 4to piso nº124",
+    "operacion": "Compra",
+    "precio": 21000
+  },
+  {
+    "localidad": "Helvecia",
+    "calle": "Publica",
+    "numero": "S/N",
+    "operacion": "Compra",
+    "precio": 165000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Mendoza",
+    "numero": "3261           P.4 D.2",
+    "operacion": "Compra",
+    "precio": 1250000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Martinez",
+    "numero": 3363,
+    "operacion": "Compra",
+    "precio": 400000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "1° de Mayo",
+    "numero": "S/N",
+    "operacion": "Alquiler",
+    "precio": 6500
+  },
+  {
+    "localidad": "Arroyo Leyes",
+    "calle": "Novena",
+    "numero": "S/Nº",
+    "operacion": "Compra",
+    "precio": 550000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de Enero",
+    "numero": "1809-11",
+    "operacion": "Alquiler",
+    "precio": 3000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "4 de Enero",
+    "numero": 1807,
+    "operacion": "Alquiler",
+    "precio": 3500
+  },
+  {
+    "localidad": "Sauce Viejo",
+    "calle": "Noruega",
+    "numero": 1269,
+    "operacion": "Compra",
+    "precio": 155000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Colon",
+    "numero": 2012,
+    "operacion": "Compra",
+    "precio": 1800000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Belgrano",
+    "numero": 3356,
+    "operacion": "Alquiler",
+    "precio": 7000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Barrio el POZO TORRE 14",
+    "numero": "PISO 9",
+    "operacion": "Compra",
+    "precio": 1250000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Victoria Ocampo E/Roverano y",
+    "numero": "T. Lubary",
+    "operacion": "Compra",
+    "precio": 250000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Rivadavia",
+    "numero": "6924 Local 2",
+    "operacion": "Alquiler",
+    "precio": 4500
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "San Martin",
+    "numero": "1869 P.4",
+    "operacion": "Compra",
+    "precio": 1600000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Estado de Israel",
+    "numero": 4982,
+    "operacion": "Compra",
+    "precio": 750000
+  },
+  {
+    "localidad": "Arroyo Aguiar",
+    "calle": "publica s/n",
+    "numero": "s/n",
+    "operacion": "Compra",
+    "precio": 1100000
+  },
+  {
+    "localidad": "Desvio Arijon",
+    "calle": "Avenida Los Eucaliptus",
+    "numero": "a 300 mts. de Ruta 11",
+    "operacion": "Compra",
+    "precio": 1300000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Av. Aristobulo del Valle",
+    "numero": 6330,
+    "operacion": "Compra",
+    "precio": 9000000
+  },
+  {
+    "localidad": "Santo Tome",
+    "calle": "Sarmiento",
+    "numero": 3869,
+    "operacion": "Compra",
+    "precio": 500000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Jujuy",
+    "numero": 3710,
+    "operacion": "Compra",
+    "precio": 1200000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "25 de Mayo D.1",
+    "numero": 2838,
+    "operacion": "Compra",
+    "precio": 132000
+  },
+  {
+    "localidad": "Sauce Viejo",
+    "calle": "Solís",
+    "numero": "entre Piedras y Tacuarí",
+    "operacion": "Compra",
+    "precio": 950000
+  },
+  {
+    "localidad": "Santa Fe",
+    "calle": "Azopardo",
+    "numero": 7101,
+    "operacion": "Compra",
+    "precio": 1500000
+  }
+];
