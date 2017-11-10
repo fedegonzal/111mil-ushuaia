de95e658ba323a28c77a6c1ac30a53c7e810326d
Viera Leonel <vieraleonel1@gmail.com>
2017-11-10 09:09:01 -0300

    Ejercicios básicos con SWING

---
+++ ProbandoSwing/src/probandoswing/ProbandoSwing.java
@@ -0,0 +1,13 @@
+package probandoswing;
+
+public class ProbandoSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }
+    
+}
